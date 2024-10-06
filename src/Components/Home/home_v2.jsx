fe962eae3087ab179b923eb9ac305ce9c9ea7142
Tai Pham <tai.pham1738@gmail.com>
2024-10-06 00:51:36 -0500

    working on v2 home

--- src/Components/Home/home_v2.jsx
+++ src/Components/Home/home_v2.jsx
@@ -53,9 +53,9 @@ export default function Home() {
                         </g>
                         </svg> */}
                 <div className="home_content grid">
-                    <SocialLink/>
                     <div className="home_img"> </div>
                     <div className="home_data">
+                    {/* <SocialLink/> */}
                       <Banner/>
                         {/* <a href='#contact' className="button hello_button button__flex"> say Hello 
                           <svg
@@ -77,14 +77,16 @@ export default function Home() {
                           </svg></a> */}
 
                     </div>
-                    {/* {scrolling icon} */}
-                    {/* <div className="home_scroll">
+                <SocialLink/>
+    
+
+                </div>
+                    <div className="home_scroll">
                       <span className="home_scroll-name">Scroll Down</span>
                       <a href="#about" className="home_scroll-button button--flex">
                       <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" style={{fill: "#0c0d0d", transform: 'msFilter'}}><path d="M11.178 19.569a.998.998 0 0 0 1.644 0l9-13A.999.999 0 0 0 21 5H3a1.002 1.002 0 0 0-.822 1.569l9 13z"></path></svg>
                       </a>
-                    </div> */}
-                </div>
+                    </div>
             </div>
         </section>
     )
