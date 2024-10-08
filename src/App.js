79e58dac33a4343aaff3160896f2ad939281c4b0
Tai Pham <tai.pham1738@gmail.com>
2024-10-08 17:54:09 -0500

    revert

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
 import React from "react";
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import "./App.css"
 import Header from './Components/Header/header';
-// import Home from './Components/Home/home';
 import About from './Components/About/about';
-import Footer from './Components/Footer/footer'
-import Skills from './Components/Skills/skills'
-import Home_v2 from './Components/Home/home_v2'
+import Footer from './Components/Footer/footer';
+import Home_v2 from './Components/Home/home_v2';
+import Resume from './Components/Resume/resume';
 function App() {
   
-
+  console.log(window.location.hostname === "localhost" ? "" : "/Personal_Site_v2")
   return (
-    <>
-      <Header/>
-      <Home_v2/>    
-      <About/>
-      <Skills/>
-      <Footer/>
-    </>
+    <Router basename = "/Personal_Site_v2">
+      <Routes>
+        <Route path="/" element={
+         <Layout>
+             <Home_v2/>    
+             <About/>
+         </Layout>
+        } />
+        <Route path="/resume" element={
+          <Layout>
+            <Resume/>
+          </Layout>
+        }/> 
+      </Routes>
+    </Router>
   )
 }
 
 export default App;
+
+function Layout({ children }) {
+  return (
+    <>
+      <Header />
+      {children}
+      <Footer />
+    </>
+  );
+}
