fdba43d02d97f01b441abfef67521da65fd360f5
Tai Pham <tai.pham1738@gmail.com>
2024-10-08 17:40:53 -0500

    redesign routes

--- src/App.js
+++ src/App.js
@@ -1,41 +1,23 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import "./App.css"
 import Header from './Components/Header/header';
+// import Home from './Components/Home/home';
 import About from './Components/About/about';
-import Footer from './Components/Footer/footer';
-import Home_v2 from './Components/Home/home_v2';
-// import Resume from './Components/Resume/resume';
+import Footer from './Components/Footer/footer'
+import Skills from './Components/Skills/skills'
+import Home_v2 from './Components/Home/home_v2'
 function App() {
   
 
   return (
-    <Router basename="/Personal_Site_v2">
-      <Routes>
-        <Route path="/" element={
-         <Layout>
-             <Home_v2/>    
-             <About/>
-         </Layout>
-        } />
-        <Route path="/resume" element={
-          <Layout>
-            {/* <Resume/> */}
-          </Layout>
-        }/> 
-      </Routes>
-    </Router>
+    <>
+      <Header/>
+      <Home_v2/>    
+      <About/>
+      <Skills/>
+      <Footer/>
+    </>
   )
 }
 
 export default App;
-
-function Layout({ children }) {
-  return (
-    <>
-      <Header />
-      {children}
-      <Footer />
-    </>
-  );
-}
