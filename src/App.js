3472b17e1e490c2f928dfec6d728e87f348e3b1c
Tai Pham <tai.pham1738@gmail.com>
2024-10-08 16:31:06 -0500

    refractor routes

--- src/App.js
+++ src/App.js
@@ -1,32 +1,41 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import "./App.css";
+import "./App.css"
 import Header from './Components/Header/header';
 import About from './Components/About/about';
 import Footer from './Components/Footer/footer';
 import Home_v2 from './Components/Home/home_v2';
-import Resume from "./Components/Resume/resume";
-
-function Layout({ children }) {
-    return (
-        <>
-            <Header />
-            {children}
-            <Footer />
-        </>
-    );
-}
-
+// import Resume from './Components/Resume/resume';
 function App() {
-    return (
-        <Router basename="/Personal_Site_v2">
-            <Routes>
-                <Route path="/" element={<Layout><Home_v2 /></Layout>} />
-                <Route path="/about" element={<Layout><About /></Layout>} />
-                <Route path="/resume" element={<Layout><Resume /></Layout>} />
-            </Routes>
-        </Router>
-    );
+  
+
+  return (
+    <Router basename="/Personal_Site_v2">
+      <Routes>
+        <Route path="/" element={
+         <Layout>
+             <Home_v2/>    
+             <About/>
+         </Layout>
+        } />
+        <Route path="/resume" element={
+          <Layout>
+            {/* <Resume/> */}
+          </Layout>
+        }/> 
+      </Routes>
+    </Router>
+  )
 }
 
 export default App;
+
+function Layout({ children }) {
+  return (
+    <>
+      <Header />
+      {children}
+      <Footer />
+    </>
+  );
+}
