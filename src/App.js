af9c65cd53d989d25d20665a522f39b67f8a5e91
Tai Pham <tai.pham1738@gmail.com>
2024-10-08 15:42:01 -0500

    removed skills page

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import "./App.css"
 import Header from './Components/Header/header';
 // import Home from './Components/Home/home';
@@ -6,18 +7,38 @@ import About from './Components/About/about';
 import Footer from './Components/Footer/footer'
 import Skills from './Components/Skills/skills'
 import Home_v2 from './Components/Home/home_v2'
+import Resume
+ from "./Components/Resume/resume";
 function App() {
   
 
   return (
-    <>
-      <Header/>
-      <Home_v2/>    
-      <About/>
-      <Skills/>
-      <Footer/>
-    </>
+    <Router>
+      <Routes>
+        <Route path="/" element={
+         <Layout>
+             <Home_v2/>    
+             <About/>
+         </Layout>
+        } />
+        <Route path="/resume" element={
+          <Layout>
+            <Resume/>
+          </Layout>
+        }/> 
+      </Routes>
+    </Router>
   )
 }
 
 export default App;
+
+function Layout({ children }) {
+  return (
+    <>
+      <Header />
+      {children}
+      <Footer />
+    </>
+  );
+}
