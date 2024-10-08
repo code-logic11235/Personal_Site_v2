2c18c1445a201596a1f4e37186932ea6e68d83a5
Tai Pham <tai.pham1738@gmail.com>
2024-10-08 16:09:05 -0500

    fix html

--- src/App.js
+++ src/App.js
@@ -2,13 +2,10 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import "./App.css"
 import Header from './Components/Header/header';
-// import Home from './Components/Home/home';
 import About from './Components/About/about';
-import Footer from './Components/Footer/footer'
-import Skills from './Components/Skills/skills'
-import Home_v2 from './Components/Home/home_v2'
-import Resume
- from "./Components/Resume/resume";
+import Footer from './Components/Footer/footer';
+import Home_v2 from './Components/Home/home_v2';
+import Resume from './Components/Resume/resume';
 function App() {
   
 
