4415d1c396da8d1761961c65dd6ffed947d18641
Tai Pham <tai.pham1738@gmail.com>
2024-10-08 22:50:12 -0500

    refractor

--- src/App.js
+++ src/App.js
@@ -1,24 +1,13 @@
-import React, { useEffect } from "react";
-import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import "./App.css"
 import Header from './Components/Header/header';
 import About from './Components/About/about';
 import Footer from './Components/Footer/footer';
 import Home_v2 from './Components/Home/home_v2';
 import Resume from './Components/Resume/resume';
+
 function App() {
-
-  const location = useLocation();
-  
-  useEffect(() => {
-    if (location.hash) {
-      const element = document.getElementById(location.hash.substring(1));
-      if (element) {
-        element.scrollIntoView({ behavior: 'smooth' });
-      }
-    }
-  }, [location]);
-
   return (
     <Router basename={window.location.hostname === "localhost" ? "" : "/Personal_Site_v2"}>
       <Routes>
@@ -35,12 +24,23 @@ function App() {
         }/> 
       </Routes>
     </Router>
-  )
+  );
 }
 
 export default App;
 
 function Layout({ children }) {
+  const location = useLocation();
+
+  useEffect(() => {
+    if (location.hash) {
+      const element = document.getElementById(location.hash.substring(1));
+      if (element) {
+        element.scrollIntoView({ behavior: 'smooth' });
+      }
+    }
+  }, [location]);
+
   return (
     <>
       <Header />
