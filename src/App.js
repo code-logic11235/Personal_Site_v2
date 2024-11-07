91a87899cba23622f54547518c0b6740c4f90217
Tai Pham <tai.pham1738@gmail.com>
2024-11-07 00:15:47 -0600

    updated path

--- src/App.js
+++ src/App.js
@@ -27,7 +27,7 @@ function App() {
          </Layout>
         } />
 
-        <Route path="/projects" element={
+        <Route path={window.location.hostname === "localhost" ? "" : "/Personal_Site_v2/projects"} element={
           <Layout 
             isModalOpen={isModalOpen} 
             setIsModalOpen={SetIsModalOpen} 
